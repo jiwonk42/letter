8af890a492de396d114c7db01df62ad197b9614b
Jiwon Kang and Kory Skarbek <kory@>
2017-01-11 09:11:31 -0800

    This will create letter page with input name that will show letter. No styling.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $(".nameInput form").submit(function(event){
+    var enteredName = $("#entered").val();
+
+    $("#letterHead").text(enteredName);
+
+    $("#hidden").show();
+    event.preventDefault();
+  });
+});
+
+// $(document).ready(function() {
+//   $("#blanks form").submit(function(event) {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var animalInput = $("input#animal").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val();
+//
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+// });
